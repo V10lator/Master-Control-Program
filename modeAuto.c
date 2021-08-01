b3de53ca00fc363f3f25635943b58288193f75c6
V10lator <v10lator@myway.de>
2021-08-01 06:28:06 +0200

    Mode auto: Show active channels.
    Signed-off-by: V10lator <v10lator@myway.de>

---
+++ modeAuto.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+
+#include "display.h"
+#include "modeAuto.h"
+#include "timerthread.h"
+
+void modeAuto(uint16_t buttons)
+{
+	int displayState = 0;
+	for(int i = 0; i < 4; i++)
+	{
+//		if(!tryLockPlug(i))
+//			continue;
+
+		if(getPlugState(i))
+			displayState |= i + 1;
+
+//		unlockPlug(i);
+	}
+
+	displaySetChannel(displayState);
+}
--- modeAuto.h
+++ modeAuto.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "display.h"
+#include "stdint.h"
 
-#define modeAuto(...) displayEnableTime()
-#define modeSwitchToAuto()
-#define modeSwitchFromAuto()
+#define modeSwitchToAuto() displayEnableTime()
+#define modeSwitchFromAuto() displaySetChannel(DISPLAY_CHANNEL_INVALID)
+
+void modeAuto(uint16_t buttons);
