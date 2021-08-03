05f015354b625e876461f6c32c61bc7c254d3c83
V10lator <v10lator@myway.de>
2021-08-03 10:50:48 +0200

    MCP2301 driver: Disable sequential mode for faster reading.
    Signed-off-by: V10lator <v10lator@myway.de>

--- mcp23017.c
+++ mcp23017.c
@@ -14,8 +14,13 @@
 
 #include "mcp23017.h"
 
-static int mcpFd = NULL;
-static const uint8_t mcp23017_addy[1] = { 0x12 };
+// [ Bank | MIRROR | SEQOP | DISSLW | HAEN | ODR | INTPOL | NOT IMPL]
+// [ BIT7 |        |       |        |      |     |        |   BIT0  ]
+// [  0   |   1    |  0    |   0    |  0   |  1  |    0   |    0    ]
+#define MCP_IOCON ((1 << 2) | (1 << 6))
+#define MCP_IOCON_BLOCK (MCP_IOCON | 1 << 5)
+
+static int mcpFd = -1;
 static uint16_t mcp_old_state = 0x0000;
 
 bool initialize_mcp23017()
@@ -34,24 +39,18 @@ bool initialize_mcp23017()
 		return false;
 	}
 
-	// [ Bank | MIRROR | SEQOP | DISSLW | HAEN | ODR | INTPOL | NOT IMPL]
-	// [ BIT7 |        |       |        |      |     |        |   BIT0  ]
-	// [  0   |   1    |  0    |   0    |  0   |  1  |    0   |    0    ]
-	uint8_t iocon = 1 << 2;
-	iocon |= 1 << 6;
-
 	uint8_t buffer[] = { 0x15,
-		iocon, 0xFF,	// 0x00 - Set ports as inputs (IODIR)
-		0xFF, 0xFF,	// 0x02 - Set active-low (IOPOL)
-		0x00, 0x00,	// 0x04 - Disable interrupts (INTEN)
-		0x00, 0x00, 	// 0x06 - Def values (DEFVAL unused)
-		0x00, 0x00,	// 0x08 - Compare against last value or def? (INTCON unused)
-		iocon, iocon,	// 0x0A - IOCON
-		0xFF, 0xFF,	// 0x0C - Enable pullups (GPPU)
-		0x00, 0x00, 	// 0x0E - INTF - READ ONLY!
-		0x00, 0x00,	// 0x10 - INTCAP -READ ONLY!
-		0x00, 0x00,	// 0x12 - Clear GPIO values (GPIO)
-		0x00, 0x00, 	// 0x14 - Clear latches (OLAT)
+		MCP_IOCON, 0xFF,	// 0x00 - Set ports as inputs (IODIR)
+		0xFF, 0xFF	,	// 0x02 - Set active-low (IOPOL)
+		0x00, 0x00,		// 0x04 - Disable interrupts (INTEN)
+		0x00, 0x00, 		// 0x06 - Def values (DEFVAL unused)
+		0x00, 0x00,		// 0x08 - Compare against last value or def? (INTCON unused)
+		MCP_IOCON, MCP_IOCON,	// 0x0A - IOCON
+		0xFF, 0xFF,		// 0x0C - Enable pullups (GPPU)
+//		0x00, 0x00, 		// 0x0E - INTF - READ ONLY!
+//		0x00, 0x00,		// 0x10 - INTCAP -READ ONLY!
+//		0x00, 0x00,		// 0x12 - Clear GPIO values (GPIO)
+//		0x00, 0x00, 		// 0x14 - Clear latches (OLAT)
 	};
 
 	// Set 16 bit mode from 8 bit
@@ -72,13 +71,32 @@ bool initialize_mcp23017()
 	}
 
 	// Now that we're in 16 bit mode: Overwrite ALL registers (full software reset)
+	// We can't disable sequential mode in between a seqential WRITE operation, so we keep it activated
 	buffer[0] = 0x00;
 	buffer[1] = 0xFF;
-	if(write(mcpFd, buffer, 23) != 23)
+	if(write(mcpFd, buffer, 15) != 15)
 	{
 		fprintf(stderr, "[MCP20317 DRIVER] Error sending init sequence!\n");
 		close_mcp23017();
 		return false;
+	}
+
+	// From here on we won't do an sequential READ/WRITE anymore, so disable it
+	buffer[0] = 0x0A;
+	buffer[1] = buffer[2] = MCP_IOCON_BLOCK;
+	if(write(mcpFd, buffer, 3) != 3)
+	{
+		fprintf(stderr, "[MCP20317 DRIVER] Error disabling sequential mode!\n");
+		close_mcp23017();
+		return false;
+	}
+
+	// Set register adress we want to read from later on
+	buffer[0] = 0x12;
+	if(write(mcpFd, buffer, 1) != 1)
+	{
+		fprintf(stderr, "[MCP20317 DRIVER] Error setting default addy!\n");
+		return mcp_old_state;
 	}
 
 	return true;
@@ -86,21 +104,15 @@ bool initialize_mcp23017()
 
 void close_mcp23017()
 {
-	if(mcpFd != NULL)
+	if(mcpFd > -1)
 	{
 		close(mcpFd);
-		mcpFd = NULL;
+		mcpFd = -1;
 	}
 }
 
 uint16_t read_mcp23017()
 {
-	if(write(mcpFd, mcp23017_addy, 1) != 1)
-	{
-		fprintf(stderr, "[MCP20317 DRIVER] Error reading GPIO pins (1)!\n");
-		return mcp_old_state;
-	}
-
 	uint16_t ret;
 	if(read(mcpFd, &ret, 2) != 2)
 	{
