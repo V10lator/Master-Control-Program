0ea70e1e8c1d1bd7f0ecb0f4b1798b11a6bbf528
V10lator <v10lator@myway.de>
2021-08-26 23:24:59 +0200

    Web UI: Style error pages.
    Signed-off-by: V10lator <v10lator@myway.de>

--- include/webui/utils.h
+++ include/webui/utils.h
@@ -10,7 +10,7 @@ static inline void httpFastReply(struct mg_connection *c, int err)
 {
 	const char *str = mg_http_status_code_str(err);
 
-	mg_http_reply(c, err, "Content-Type: text/html; charset=utf-8" HTTP_NEWLINE CACHE_CONTROL_NOCACHE, "<html><head><title>%i %s</title></head><body><center><h1>%i %s</h1><hr>" HTTP_STRING  "</center></body></html><!-- We need some padding cause some browsers are stupid ant want to render their shit instead of this beautiful page we worked so much on... -->" HTTP_NEWLINE,
+	mg_http_reply(c, err, "Content-Type: text/html; charset=utf-8" HTTP_NEWLINE CACHE_CONTROL_NOCACHE, "<html><head><title>%i %s</title><link href=\"/css/technics.css\" rel=\"stylesheet\"></head><body><center><h1>%i %s</h1><hr>" HTTP_STRING  "</center></body></html><!-- We need some padding cause some browsers are stupid ant want to render their shit instead of this beautiful page we worked so much on... -->" HTTP_NEWLINE,
 		err, str,
 		err, str
 	);
