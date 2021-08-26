afbf352432136b7b27a7dd704c4d613f6c94c137
V10lator <v10lator@myway.de>
2021-08-26 13:20:16 +0200

    Web UI: Add newline at end of fast HTTP reply.
    Signed-off-by: V10lator <v10lator@myway.de>

--- include/webui/utils.h
+++ include/webui/utils.h
@@ -9,7 +9,7 @@ static inline void httpFastReply(struct mg_connection *c, int err)
 {
 	const char *str = mg_http_status_code_str(err);
 
-	mg_http_reply(c, err, "Content-Type: text/html; charset=utf-8\r\n" CACHE_CONTROL_NOCACHE, "<html><head><title>%i %s</title></head><body><center><h1>%i %s</h1><hr>" HTTP_STRING  "</center></body></html><!-- We need some padding cause some browsers are stupid ant want to render their shit instead of this beautiful page we worked so much on... -->",
+	mg_http_reply(c, err, "Content-Type: text/html; charset=utf-8\r\n" CACHE_CONTROL_NOCACHE, "<html><head><title>%i %s</title></head><body><center><h1>%i %s</h1><hr>" HTTP_STRING  "</center></body></html><!-- We need some padding cause some browsers are stupid ant want to render their shit instead of this beautiful page we worked so much on... -->\r\n",
 		err, str,
 		err, str
 	);
@@ -17,5 +17,5 @@ static inline void httpFastReply(struct mg_connection *c, int err)
 
 static inline void httpReplyJson(struct mg_connection *c, void *data) // TODO
 {
-	mg_http_reply(c, 200, "Content-Type: application/json\r\n" CACHE_CONTROL_NOCACHE, "TODO");
+	mg_http_reply(c, 200, "Content-Type: application/json\r\n" CACHE_CONTROL_NOCACHE, "TODO\r\n");
 }
