75c1684d12ef329c194552180b55ac3a8d66bcc8
V10lator <v10lator@myway.de>
2021-08-27 12:04:35 +0200

    Small Web UI optimisation.
    Signed-off-by: V10lator <v10lator@myway.de>

--- include/webui/utils.h
+++ include/webui/utils.h
@@ -6,41 +6,13 @@
 #define HTTP_NEWLINE "\r\n"
 #define CACHE_CONTROL_NOCACHE "Cache-Control: no-store, max-age=0" HTTP_NEWLINE
 
+extern const char *htmlErrorTemplate;
+
 static inline void httpFastReply(struct mg_connection *c, int err)
 {
 	const char *str = mg_http_status_code_str(err);
 
-	mg_http_reply(c, err, "Content-Type: text/html; charset=utf-8" HTTP_NEWLINE CACHE_CONTROL_NOCACHE,
-"<!DOCTYPE html>"
-"<html>"
-"<head>"
-	"<meta charset=\"utf-8\">"
-	"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
-	"<title>%i %s</title>"
-	"<link href=\"/css/bootstrap.min.css\" rel=\"stylesheet\" />"
-	"<link href=\"/css/technics.css\" rel=\"stylesheet\" />"
-"</head>"
-"<body>"
-	"<div class=\"container bg-primary text-center\">"
-		"<div class=\"row\">"
-			"<div class=\"col-sm-100 p-5 bg-secondary text-center\">"
-				"<h1>Technics SH-4060</h1>"
-			"</div>"
-		"</div>"
-		"<div class=\"row\">"
-			"<div class=\"col-sm-100\">"
-				"<h2>%i %s</h2>"
-				"<hr>"
-				HTTP_STRING
-			"</div>"
-		"</div>"
-	"</div>"
-"</body>"
-"</html>"
-"<!-- We need some padding cause some browsers are stupid ant want to render their shit instead of this beautiful page we worked so much on... -->" HTTP_NEWLINE,
-		err, str,
-		err, str
-	);
+	mg_http_reply(c, err, "Content-Type: text/html; charset=utf-8" HTTP_NEWLINE CACHE_CONTROL_NOCACHE, htmlErrorTemplate, err, str, err, str);
 }
 
 static inline void httpReplyJson(struct mg_connection *c, void *data) // TODO
